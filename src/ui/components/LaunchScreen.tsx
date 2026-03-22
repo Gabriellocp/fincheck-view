33e7b1480c889833795b913b8325a9f742892fe2
gabriellocp <gabriel_campista@hotmail.com>
2026-03-22 14:02:32 -0300

    fix: Add z-index on LaunchScreen

--- src/ui/components/LaunchScreen.tsx
+++ src/ui/components/LaunchScreen.tsx
@@ -9,7 +9,7 @@ export function LaunchScreen({ isLoading }: LaunchScreenProps) {
   return (
     <Transition show={isLoading}>
       <div className="
-      transition duration-300 ease-in data-closed:opacity-0
+      z-50 transition duration-300 ease-in data-closed:opacity-0
       bg-teal-900 fixed top-0 left-0 h-full w-full grid place-items-center">
         <div className='flex flex-col gap-2 justify-center items-center'>
           <Logo className="text-white h-10" />
