4122cd49d459bbc8fc907dccbebfe9b16d1b4385
gabriellocp <gabriel_campista@hotmail.com>
2026-03-28 17:35:11 -0300

    fix: Fix Modal issues

--- src/ui/components/Modal/index.tsx
+++ src/ui/components/Modal/index.tsx
@@ -1,31 +1,39 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
+import { cn } from "../../../app/utils/cn";
 import { Content } from "./Content";
 
 interface ModalProps {
   open: boolean,
   children: React.ReactNode,
   title: string,
-  trailingAction?: React.ReactNode
+  trailingAction?: React.ReactNode,
+  onClose?: () => void
 }
 
-export function Modal({ open, title, trailingAction, children }: ModalProps) {
+export function Modal({ open, title, trailingAction, onClose, children }: ModalProps) {
   return (
-    <Dialog.Root open={open}>
+    <Dialog.Root open={open} onOpenChange={onClose}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/80 backdrop-blur-xs z-50 data-[state=open]:animate-overlay-show" />
-        <Dialog.Content className={`
-        fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 data-[state=open]:animate-content-show
+        <Dialog.Overlay className={cn(`
+        fixed inset-0 bg-black/80 backdrop-blur-xs z-50
+        `, "data-[state=open]:animate-overlay-show"
+        )} />
+        <Dialog.Content className={cn(`
+        fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
         bg-white rounded-2xl p-6 space-y-10 z-50 outline-none shadow-[0px_11px_20px_0px_rgba(0,0,0,0.1)]
         w-full max-w-100
-        `}>
-          <header className="h-12 flex items-center justify-between text-gray-800">
-            <button className="w-12 h-12">
-              <Cross2Icon className="w-6 h-6" />
-            </button>
-            <span className="text-lg font-bold tracking-[-1px]">{title}</span>
-            <div className="w-12 h-12 flex items-center justify-center">{trailingAction}</div>
-          </header>
+        `, "data-[state=open]:animate-content-show")}>
+          <Dialog.Title>
+
+            <header className="h-12 flex items-center justify-between text-gray-800">
+              <button onClick={onClose} className="w-12 h-12 flex items-center justify-center outline-none">
+                <Cross2Icon className="w-6 h-6" />
+              </button>
+              <span className="text-lg font-bold tracking-[-1px]">{title}</span>
+              <div className="w-12 h-12 flex items-center justify-center">{trailingAction}</div>
+            </header>
+          </Dialog.Title>
           <div>
             {children}
           </div>
